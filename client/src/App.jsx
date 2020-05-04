3459b9bf391688a16e0e8c1178d896db1fdcee0b
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-05-04 16:58:21 +0100

    feat: displays users from mongodb on index page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  return <></>
+  const [users, setUsers] = useState(null)
+
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/users')
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  const submitUsername = () => {
+    if (username === '') {
+      alert('Please enter a username')
+      return
+    }
+    axios
+      .post('http://localhost:8000/api/users/user', {
+        name: username,
+      })
+      .then(() => {
+        console.log('Account created successfully')
+      })
+      .catch(() => {
+        console.log('Could not create account')
+      })
+  }
+
+  const noUsers = () => {
+    if (users === null) {
+      return <p>Loading...</p>
+    }
+    if (users.length === 0) {
+      return <p>No users</p>
+    }
+  }
+
+  return (
+    <>
+      {noUsers() ||
+        users.map((u) => {
+          return <p>{u.name}</p>
+        })}
+      <form onSubmit={submitUsername}>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Enter your username"
+        />
+        <input type="submit" />
+      </form>
+    </>
+  )
 }
 
 export default App
