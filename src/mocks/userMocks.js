3bf452f495cb9f64f249a1c0d44a331a4ec550f4
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-06-01 11:53:38 +0100

    test: create user mocks

---
+++ src/mocks/userMocks.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    name: 'Chad',
+    email: 'whiteClaw@aol.com',
+    password: 'password',
+    register_date: 1591008680547,
+  },
+  {
+    name: 'Ted',
+    email: 'deathToChads@mock.com',
+    password: 'password',
+    register_date: 1591008680547,
+  },
+  {
+    name: 'Penny',
+    email: 'iLuvCake@mock.com',
+    password: 'password',
+    register_date: 1591008680547,
+  },
+]
+
+export default users
