b1c6797df80d320972097150cad3f8757a6a93ee
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-06-10 22:46:02 +0100

    fix: async weirdness when saving user twice

--- tests/routes/users.test.js
+++ tests/routes/users.test.js
@@ -3,7 +3,6 @@ import chaiHttp from 'chai-http'
 import { it, beforeEach, describe } from 'mocha'
 import User from '../../src/models/User'
 import app from '../../server'
-'
 
 const { expect } = chai
 
@@ -18,23 +17,6 @@ describe('Users', () => {
 
   describe('GET /user', () => {
     it('it should respond with empty array if no users', (done) => {
-      chai
-        .request(app)
-        .get('/users')
-        .end((err, res) => {
-          expect(res.status).to.equal(200)
-          expect(res.body).to.be.an('array')
-          expect(res.body.length).to.equal(0)
-          done()
-        })
-    })
-    it('it should get one user', (done) => {
-      const user = new User({ name: 'Harry', email: 'harry@harry.com', password: 'harry'})
-      
-      user.save((err) => {
-        if (err) { console.log(err) }
-      })
-      
       chai
         .request(app)
         .get('/users')
